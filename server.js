1cf86c2bcde838ea1d70da41beb03041de484426
Abhishek Yadav <145394772+Abhishekcsds@users.noreply.github.com>
2025-07-19 00:55:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
+// Port 3000
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
